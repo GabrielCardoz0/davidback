752c5cb4e9f0f4f88eb5162983f33fdfb04a32d2
GabrielCardoz0 <gabriel.scardozo7@gmail.com>
2024-12-07 11:55:54 -0300

    fix backup

--- src/controllers/backup.js
+++ src/controllers/backup.js
@@ -1,18 +1,25 @@
 import { PrismaClient } from "@prisma/client";
-import {exec} from 'child_process';
+import { exec } from 'child_process';
 import fs from 'fs';
 
 const prisma = new PrismaClient();
 
-export default async function backupDb(req,res) {
-    // const outputFile = './src/files/backup.sql';
-    const outputFile = "~/backups/backup.sql";
+export default async function backupDb(req, res) {
+    const outputFile = "/var/www/beautytag-back.com/backups/backup.sql";
     try {
         await generateDumpFile(outputFile);
-        return res.download(outputFile);
+        
+        // Aguarde até que o dump seja criado antes de fazer o download
+        res.download(outputFile, (err) => {
+            if (err) {
+                console.error(`Erro ao enviar o arquivo: ${err}`);
+                return res.status(500).json({ message: "Erro ao enviar o arquivo de backup" });
+            }
+            console.log(`Arquivo de backup enviado: ${outputFile}`);
+        });
     } catch (error) {
         console.log(error);
-        res.status(500).json({message: "Erro ao realizar o backup do banco de dados"});
+        res.status(500).json({ message: "Erro ao realizar o backup do banco de dados" });
     } finally {
         setTimeout(() => {
             console.log("Removendo arquivo de backup");
@@ -21,9 +28,8 @@ export default async function backupDb(req,res) {
     }
 };
 
-
 async function generateDumpFile(outputFile) {
-    const {DB_USER,DB_PASSWORD,DB_HOST,DB_PORT,DB_NAME, SUDO_PASSWORD} = process.env;
+    const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, SUDO_PASSWORD } = process.env;
 
     const username = DB_USER;
     const password = DB_PASSWORD;
@@ -35,24 +41,22 @@ async function generateDumpFile(outputFile) {
     await prisma.$connect();
 
     // Faça o dump do banco de dados usando pg_dump
-    const child = `echo ${SUDO_PASSWORD} | sudo -S PGPASSWORD='${password}' pg_dump -U ${username} -h ${host} -p ${port} ${dbname} > ${outputFile}`;
+    const dumpCommand = `PGPASSWORD='${password}' pg_dump -U ${username} -h ${host} -p ${port} ${dbname} > ${outputFile}`;
 
-    const dumpCommand = `echo ${SUDO_PASSWORD} | sudo -S -u postgres PGPASSWORD='${password}' sudo -u ${username} pg_dump -U ${username} -h ${host} -p ${port} ${dbname} > ${outputFile}
-`;
-
-    exec(dumpCommand, (error, stdout, stderr) => {
-        if (error) {
-        console.error(`Erro ao executar o pg_dump: ${error.message}`);
-        return;
-        }
-        if (stderr) {
-        console.error(`stderr: ${stderr}`);
-        return;
-        }
-        console.log(`Dump do banco de dados criado com sucesso em ${outputFile}`);
+    return new Promise((resolve, reject) => {
+        exec(dumpCommand, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Erro ao executar o pg_dump: ${error.message}`);
+                return reject(error);
+            }
+            if (stderr) {
+                console.error(`stderr: ${stderr}`);
+                return reject(new Error(stderr));
+            }
+            console.log(`Dump do banco de dados criado com sucesso em ${outputFile}`);
+            resolve();
+        });
     });
-
-    await prisma.$disconnect();
 };
 
 function removeFile(filePath) {
@@ -64,4 +68,3 @@ function removeFile(filePath) {
         console.log(`Arquivo removido com sucesso: ${filePath}`);
     });
 }
-
